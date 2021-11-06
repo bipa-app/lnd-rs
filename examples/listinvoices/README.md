f89e1e349db81f0b869ea9735be83bcd3990bdcb
Dave Jones <sect62@gmail.com>
2021-11-06 13:54:32 -0500

    Add list_invoices call (#21)
    * Add list_invoices call
    
    * Added example for pulling invoices
    
    * Requested edits for pull request

---
+++ examples/listinvoices/README.md
@@ -0,0 +1,8 @@
+This example connects to LND and pulls 100 invoices every 15 seconds starting with the first.
+
+To use it, you will need to set the $LND_URL environment variable to the fully qualified domain name
+of your LND node, including port, like this:
+
+export LND_URL="mynode.example.com:10009"
+
+It will also look for your *admin.macaroon* and *tls.cert* files in the folder you run from as well.
---
+++ examples/listinvoices/src/main.rs
@@ -0,0 +1,55 @@
+use lnd;
+use std::fs;
+use std::env;
+use std::thread;
+
+#[tokio::main]
+async fn main() {
+
+    //Get the macaroon and cert files
+    let macaroon = fs::read("admin.macaroon").unwrap();
+    let cert = fs::read("tls.cert").unwrap();
+
+    //Get the url connection string of the lnd node
+    let env_lnd_url = env::var("LND_URL");
+    if env_lnd_url.is_err() {
+        println!("The $LND_URL environment variable could not be found.\n");
+        std::process::exit(1);
+    }
+    let node_address = "https://".to_owned() + env_lnd_url.unwrap().as_str();
+
+    //Make the connection to LND
+    let mut lightning = match lnd::Lnd::connect_with_macaroon(node_address.clone(), &cert, &macaroon).await {
+        Ok(lndconn) => lndconn,
+        Err(e) => {
+            println!("Could not connect to: {} using tls and macaroon", node_address);
+            eprintln!("{}", e);
+            std::process::exit(1);
+        },
+    };
+
+    //Enter a loop of pulling invoices
+    let mut current_index: u64 = 0;
+    loop {
+
+        //Get a list of invoices
+        match lnd::Lnd::list_invoices(&mut lightning, false, current_index.clone(), 100, false).await {
+            Ok(response) => {
+                for invoice in response.invoices {
+                    println!("{} - {} - {} sats", invoice.add_index, invoice.settle_date, invoice.amt_paid_sat);
+                    if invoice.add_index > current_index {
+                        current_index = invoice.add_index;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        }
+
+        //Current index
+        println!("Current index: {}", current_index);
+
+        thread::sleep(std::time::Duration::from_millis(15000));
+    }
+}
