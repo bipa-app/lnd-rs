301e638baeef5b9d3fc75cc35938b1bb5d1d3892
Lucas Sunsi Abreu <lsunsi@pm.me>
2022-02-11 15:00:52 -0300

    Update protos and include invoicesrpc

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 const BUILD_DIR: &'static str = "src/gen";
 
 const PROTOS_NS: &'static str = "protos";
-const PROTOS: [&'static str; 2] = ["protos/rpc.proto", "protos/router.proto"];
+const PROTOS: &[&'static str] = &[
+    "protos/invoices.proto",
+    "protos/lightning.proto",
+    "protos/router.proto",
+];
 
 fn main() -> Result<(), std::io::Error> {
     std::fs::create_dir_all(BUILD_DIR)?;
